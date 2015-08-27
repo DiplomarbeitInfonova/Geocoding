5851e209110e720159594ee7d1d1b275d33f067f
neupaa11 <patzineubi@jmrh>
2015-08-27 20:38:02 +0200

    google-maps-services-java dependency hinzugefügt / Verbesserung Roads API

--- src/main/java/bl/SnapToRoads.java
+++ src/main/java/bl/SnapToRoads.java
@@ -1,6 +1,11 @@
 package bl;
 
+import beans.Location;
+import com.google.maps.GeoApiContext;
+import com.google.maps.RoadsApi;
+import com.google.maps.model.SnappedPoint;
 import java.io.InputStream;
+import java.util.LinkedList;
 
 /**
  *
@@ -9,41 +14,41 @@ import java.io.InputStream;
  */
 public class SnapToRoads {
 
-    
+    private LinkedList<Location> list;
 
-    public SnapToRoads(InputStream inputStream) {
-       
+    public SnapToRoads(LinkedList<Location> liste) {
+       this.list = liste;
     }
 
-    /**
- * Snaps the points to their most likely position on roads using the Roads API.
- */
+//    /**
+// * Snaps the points to their most likely position on roads using the Roads API.
+// */
 //private LinkedList<Location> snapToRoads(GeoApiContext context) throws Exception {
 //    LinkedList<Location> snappedPoints = new LinkedList<Location>();
 //
 //    int offset = 0;
-//    while (offset < mCapturedLocations.size()) {
+//    while (offset < list.size()) {
 //        // Calculate which points to include in this request. We can't exceed the APIs
 //        // maximum and we want to ensure some overlap so the API can infer a good location for
 //        // the first few points in each request.
 //        if (offset > 0) {
-//            offset -= PAGINATION_OVERLAP;   // Rewind to include some previous points
+//            offset -= 100;   // Rewind to include some previous points
 //        }
 //        int lowerBound = offset;
-//        int upperBound = Math.min(offset + PAGE_SIZE_LIMIT, mCapturedLocations.size());
+//        int upperBound = Math.min(offset + 100, list.size());
 //
 //        // Grab the data we need for this page.
-//        LatLng[] page = mCapturedLocations
+//        Location[] page = list
 //                .subList(lowerBound, upperBound)
-//                .toArray(new LatLng[upperBound - lowerBound]);
+//                .toArray(new Location[upperBound - lowerBound]);
 //
 //        // Perform the request. Because we have interpolate=true, we will get extra data points
 //        // between our originally requested path. To ensure we can concatenate these points, we
 //        // only start adding once we've hit the first new point (i.e. skip the overlap).
-//        SnappedPoint[] points = RoadsApi.snapToRoads(context, true, page).await();
+//        //LinkedList<Location> points = SnapToRoads.snapToRoads(context, true, page).await();
 //        boolean passedOverlap = false;
-//        for (SnappedPoint point : points) {
-//            if (offset == 0 || point.originalIndex >= PAGINATION_OVERLAP - 1) {
+//        for (Location point : points) {
+//            if (offset == 0 || point.getxKoord() >= 100 - 1) {
 //                passedOverlap = true;
 //            }
 //            if (passedOverlap) {
