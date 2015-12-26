0aa5162ec4d279f30301d95f60914ac1fa1ae2a4
zeidoa11 <d.zeiler7@gmail.com>
2015-12-26 15:44:36 +0100

    Aufrufe von Geocoding wurden reduziert, namen im höhendiagramm werden nur nach bedarf geholt

--- src/main/java/beans/Location.java
+++ src/main/java/beans/Location.java
@@ -6,8 +6,7 @@ import bl.StringUtils;
  *
  * @author David
  */
-public class Location
-{
+public class Location {
 
     private String name;
     private double xKoord;
@@ -15,20 +14,25 @@ public class Location
     private double hoehe;
 
     public Location() {
-        }
-
-    public double getHoehe()
-    {
-        return hoehe;
     }
 
-    public void setHoehe(double hoehe)
-    {
+    public double getHoehe() {
+        return hoehe;
+    }
+//
+    
+    public void setHoehe(double hoehe) {
         this.hoehe = hoehe;
     }
 
-    public Location(String name, double xKoord, double yKoord, double hoehe)
-    {
+    public Location(double[] koordinaten) {
+        this.name="";
+        this.hoehe=0;
+        this.xKoord = koordinaten[0];
+        this.yKoord = koordinaten[1];
+    }
+
+    public Location(String name, double xKoord, double yKoord, double hoehe) {
         this.name = StringUtils.correctLettersForAPI(name);
         this.xKoord = xKoord;
         this.yKoord = yKoord;
@@ -41,46 +45,41 @@ public class Location
 //        this.xKoord = xKoord;
 //        this.yKoord = yKoord;
 //    }
-
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public double getxKoord()
-    {
+    public double getxKoord() {
         return xKoord;
     }
 
-    public void setxKoord(double xKoord)
-    {
+    public void setxKoord(double xKoord) {
         this.xKoord = xKoord;
     }
 
-    public double getyKoord()
-    {
+    public double getyKoord() {
         return yKoord;
     }
 
-    public void setyKoord(double yKoord)
-    {
+    public void setyKoord(double yKoord) {
         this.yKoord = yKoord;
     }
 
-    
-    public String toCSVRow(){
-        return this.name+";"+this.xKoord+";"+this.yKoord+";"+this.hoehe;
+    public String toCSVRow() {
+        return this.name + ";" + this.xKoord + ";" + this.yKoord + ";" + this.hoehe;
+    }
+
+    public double[] getKoordArray(){
+        return new double[]{xKoord,yKoord};
     }
     
     
     @Override
-    public String toString()
-    {
+    public String toString() {
         return String.format("Location=\n\tName: %s\n\t x-Koord: %f \n\t y-Koord: %f \n\t Hoehe: %f", this.name, this.xKoord, this.yKoord, this.hoehe);
     }
 
